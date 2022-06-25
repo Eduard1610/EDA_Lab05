0dbeb23e8b4e478ce9f4faa41481c9404ac72321
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-06-25 18:32:58 -0500

    Implementando la clase AVLTree básica

---
+++ 5.3_ArbolAVL/AVLTree.java
@@ -0,0 +1,21 @@
+public class AVLTree <E extends Comparable<E>> {
+    
+    // ATRIBUTO DE LA CLASE AVL
+	private Node root;
+
+    // CONSTRUCTORES
+	public AVL() {
+		this.root = null;
+	}
+
+    // MÉTODOS DEL ÁRBOL
+	
+	// Para saber si el árbol está vacío
+	public boolean isEmpty() {
+		return this.root == null;
+	}
+
+    public void insert(E x) {
+		
+	}
+}
