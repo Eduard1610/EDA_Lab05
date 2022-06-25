19be65f25f9477fd7385ebaeb7425a7769bd843e
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-06-25 18:37:35 -0500

    Implementando la clase Node dentro la clase del AVL y sustituyendo letra E por T (de los datos genéricos)

--- 5.3_ArbolAVL/AVLTree.java
+++ 5.3_ArbolAVL/AVLTree.java
@@ -1,5 +1,26 @@
-public class AVLTree <E extends Comparable<E>> {
-    
+public class AVLTree <T extends Comparable<T>> {
+
+    class Node {
+		// No es necesario que Node tenga <T> Ya que esta clase (Node) está inculuida en una clase genérica
+		protected T data;
+		protected Node left;
+		protected Node right;
+		protected int fb; // Factor de balance
+		
+		public Node(T data, Node left, Node right) {
+			this.data = data;
+			this.left = left;
+			this.right = right;
+			this.fb = 0;
+		}
+	
+		// Para los nuevos nodos hoja (No tienen hijos)
+		public Node(T data) {
+			// Llamamos al constructor
+			this(data, null, null);
+		}
+	}
+
     // ATRIBUTO DE LA CLASE AVL
 	private Node root;
 
@@ -15,7 +36,7 @@ public class AVLTree <E extends Comparable<E>> {
 		return this.root == null;
 	}
 
-    public void insert(E x) throws ItemDuplicated{
+    public void insert(T x) throws ItemDuplicated{
 		
 	}
 }
