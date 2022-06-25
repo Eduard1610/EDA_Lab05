a1c4d6c4918036be5c87aad7b13a20c58c9d93e7
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-06-25 18:41:02 -0500

    Implementando lógica para la inserción y ordenamiento AVL

--- 5.3_ArbolAVL/AVLTree.java
+++ 5.3_ArbolAVL/AVLTree.java
@@ -23,6 +23,7 @@ public class AVLTree <T extends Comparable<T>> {
 
     // ATRIBUTO DE LA CLASE AVL
 	private Node root;
+    private boolean height; // Si pasa a true es cambio de altura, false lo contrario
 
     // CONSTRUCTORES
 	public AVL() {
@@ -36,7 +37,51 @@ public class AVLTree <T extends Comparable<T>> {
 		return this.root == null;
 	}
 
-    public void insert(T x) throws ItemDuplicated{
-		
+	public void insert(T x) throws ItemDuplicated {
+		this.height = false; // Por si el nodo no se inserta
+		this.root = insertRec(x, this.root);
+	}
+
+    private Node insertRec(E x, Node current) throws ItemDuplicated {
+		Node res = current;
+		if(current == null) {
+			// En el caso base cuando hay inserción debemos consultar el cambio de altura
+			this.height = true; 
+			res = new Node(x);
+		}
+		else {
+			// resC == resultado de la comparación
+			int resC = current.data.compareTo(x);
+			if (resC == 0)
+				throw new ItemDuplicated("El dato " + x + " ya fue insertado antes.");
+			if (resC < 0) {
+				res.right = insertRec(x, current.right);
+				if (this.height) { // Si existe un cambio en la altura
+					switch(res.fb) {// Casos de fb si insertamos por la derecha
+					// En inserción si fb pasa de -1 o 1 a 0 entonces no hay cambio de altura
+					case -1: res.fb = 0; this.height = false; break; // Se cancela el recálculo de altura
+					case 0: res.fb = 1; this.height = true; break; // Seguimos recalculando
+					case 1: //res.fb = 2; debemos balancear el árbol
+						res = balanceToLeft(res);
+						this.height = false; // Cuando hacemos la rotación el árbol debe quedar balanceado
+					}
+				}
+			}
+			else {
+				res.left = insertRec(x, current.left);
+				if(this.height) { // Si existe un cambio en la altura
+					switch(res.fb) { // Casos de fb si insertamos por la derecha
+					// En inserción si fb pasa de -1 o 1 a 0 entonces no hay cambio de altura
+					case 1: res.fb = 0; this.height = false; break;// Se cancela el rec�lculo de altura
+					case 0: res.fb = -1; this.height = true; break; // Seguimos recalculando
+					case -1: //res.fb = -2; debemos balancear el árbol
+						res = balanceToRight(res);
+						this.height = false; // Cuando hacemos la rotación el árbol debe quedar balanceado
+					}
+				}
+			}
+
+		}
+		return res;
 	}
 }
